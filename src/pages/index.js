df22b5d449541507c338ed22ba4520700fa8beca
Lucca-Chan <luccpao90@gmail.com>
2026-07-14 10:50:44 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      lá ele.{" "}
+      afonso.{" "}
       <Link href="/about">
         About
       </Link>
